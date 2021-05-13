da43527b9bd4cb70cfd352517c4e6ef3280e789d
Dmitry Koverko <dmitry.koverko@gmail.com>
2021-05-13 23:19:41 +0300

    add log

--- arbitrajlibrary/src/main/java/com/kawka/arbitrajlibrary/app/App.java
+++ arbitrajlibrary/src/main/java/com/kawka/arbitrajlibrary/app/App.java
@@ -1,14 +1,17 @@
 package com.kawka.arbitrajlibrary.app;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.kawka.arbitrajlibrary.UserData;
 
 public class App extends Application {
 
+    private final String TAG = App.class.getName();
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Log.d(TAG, "onCreate Application class");
     }
 }
